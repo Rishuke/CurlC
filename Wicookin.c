46def0f96c57a82a37cedcc31d9c193681236dc4
Rishi <balasubramaniam.rishi@gmail.com>
2023-05-07 21:38:46 +0200

    add parametre Wicookin.c

--- Wicookin.c
+++ Wicookin.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
   CURL *curl;
   CURLcode res;
-
+  char chaine1[100] = "https://pokebuildapi.fr/api/v1";
+  char chaine2[100] = "";
+  
+  strcat(chaine1,chaine2);
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"); // Remplacez l'URL par l'URL de votre base de données
+  	
+  	for (int i = 1; i < argc; i++) {
+        strcat(chaine2,argv[i]);
+        
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, strcat(chaine1,chaine2)); // Remplacez l'URL par l'URL de votre base de données
     //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
     //"jdbc:mysql://localhost:3306/Meal?user=admin&password=password"
     //https://pokebuildapi.fr/api/v1/pokemon
