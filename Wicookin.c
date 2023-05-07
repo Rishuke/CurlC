8515a084a8c44a44c99bd56aba09961db068297f
Rishi <balasubramaniam.rishi@gmail.com>
2023-05-07 16:29:17 +0200

    add Wicookin.c

--- Wicookin.c
+++ Wicookin.c
@@ -8,9 +8,10 @@ int main(void)
 
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts/1"); // Remplacez l'URL par l'URL de votre base de données
-    
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"); // Remplacez l'URL par l'URL de votre base de données
+    //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
     //"jdbc:mysql://localhost:3306/Meal?user=admin&password=password"
+    //https://pokebuildapi.fr/api/v1/pokemon
     res = curl_easy_perform(curl);
 
     if(res != CURLE_OK)
