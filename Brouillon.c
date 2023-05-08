c8bbde7284dbfbf35299a7419aca13bc0d2b640f
Rishi <balasubramaniam.rishi@gmail.com>
2023-05-08 17:35:15 +0200

    add Graphique.c modification parametre

--- Brouillon.c
+++ Brouillon.c
@@ -1,54 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 #include <gtk/gtk.h>
 
-GtkWidget *window1, *window2, *window3;
+const gchar* texte_saisi = NULL;
 
-void open_window2() {
-    window2 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window2), "Window 2");
-    gtk_window_set_default_size(GTK_WINDOW(window2), 200, 200);
-    gtk_widget_show_all(window2);
-}
-
-void open_window3() {
-    window3 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window3), "Window 3");
-    gtk_window_set_default_size(GTK_WINDOW(window3), 200, 200);
-    gtk_widget_show_all(window3);
-}
-
-void close_window(GtkWidget *widget, gpointer data) {
-    gtk_widget_destroy(data);
+void on_button_clicked(GtkWidget* widget, gpointer data) {
+    GtkWidget* entry = (GtkWidget*)data;
+    texte_saisi = gtk_entry_get_text(GTK_ENTRY(entry));
+    g_print("Texte saisi : %s\n", texte_saisi);
 }
 
 int main(int argc, char *argv[]) {
-    GtkWidget *button1, *button2, *button3;
-    GtkWidget *vbox;
+    // Définition de la locale pour l'affichage
+    setlocale(LC_ALL, "en_US.UTF-8");
 
+    // Initialisation de GTK
     gtk_init(&argc, &argv);
 
-    window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window1), "Window 1");
-    gtk_window_set_default_size(GTK_WINDOW(window1), 200, 200);
+    // Création de la fenêtre et des widgets
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *grid = gtk_grid_new();
+    GtkWidget *entry = gtk_entry_new();
+    GtkWidget *button = gtk_button_new_with_label("Valider");
 
-    button1 = gtk_button_new_with_label("Open Window 2");
-    g_signal_connect(button1, "clicked", G_CALLBACK(open_window2), NULL);
+    // Ajout des widgets à la grille et connexion du signal clicked du bouton
+    gtk_container_add(GTK_CONTAINER(window), grid);
+    gtk_grid_attach(GTK_GRID(grid), entry, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), entry);
 
-    button2 = gtk_button_new_with_label("Open Window 3");
-    g_signal_connect(button2, "clicked", G_CALLBACK(open_window3), NULL);
+    // Affichage de la fenêtre
+    gtk_widget_show_all(window);
 
-    button3 = gtk_button_new_with_label("Close Window 1");
-    g_signal_connect(button3, "clicked", G_CALLBACK(close_window), window1);
-
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), button1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), button2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), button3, TRUE, TRUE, 0);
-
-    gtk_container_add(GTK_CONTAINER(window1), vbox);
-
-    gtk_widget_show_all(window1);
-
+    // Boucle principale de GTK
     gtk_main();
 
-    return 0;
+    // Affichage du texte saisi dans la console
+    if (texte_saisi != NULL) {
+        printf("Le texte saisi est : %s\n", texte_saisi);
+    }
+
+    return EXIT_SUCCESS;
 }
+
--- Graphique.c
+++ Graphique.c
@@ -6,6 +6,7 @@
 #include <curl/curl.h>
 
 GtkWidget *window1, *window2, *window3;
+char *chaine;
 
 void close_window(GtkWidget *widget, gpointer data) {
     gtk_widget_destroy(data);
@@ -37,19 +38,38 @@ void on_button_nbparam(GtkButton *button ,gpointer user_data) {
     printf("Le résultat est : %d\n", global_result);
 }
 
-void on_button_param(GtkButton *button, gpointer user_data) {
+
+
+void on_button_param(const gchar* tab, gpointer user_data) {
     GtkWidget *entry = GTK_WIDGET(user_data);
     const gchar *param = gtk_entry_get_text(GTK_ENTRY(entry));
     g_print("Le nom de parametre est : %s\n", param);
+    printf("%d\n", global_result);
+    int a = 0;
     
+    if(a < global_result){
+    	chaine = strcpy(chaine,(char*)param);
+    	a++;
+    	g_print("Le nom de parametre chaine est : %s\n", chaine);
+    }
+  
     
 }
+
+
 
 void open_window3() {
     window3 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window3), "Requete de l'API");
     gtk_window_set_default_size(GTK_WINDOW(window3), 400, 400);
+    GtkWidget *label;
+    GtkWidget *vbox3;
     
+    vbox3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window3), vbox3);
+    
+   	label = gtk_label_new("Hello, world!");
+    gtk_box_pack_start(GTK_BOX(vbox3), label, TRUE, TRUE, 0);
     
     
     gtk_widget_show_all(window3);
@@ -103,13 +123,57 @@ void open_window2() {
     
 }
 
+void requete(GtkWidget *label, int count, ...) {
+	
+	
+    CURL *curl;
+    CURLcode res;
+    char chaine1[100] = "https://pokebuildapi.fr/api/v1";
+    char chaine2[100] = "";
+
+    strcat(chaine1, chaine2);
+    curl = curl_easy_init();
+    va_list ap;
+    va_start(ap, count);
+    if(curl) {
+        for (int k = 0; k < count; k++) {
+            char *str = va_arg(ap,char*);
+           	
+            strcat(chaine2, str);
+        }
+        va_end(ap);
+
+        char url[200];
+        strcpy(url, strcat(chaine1, chaine2));
+        curl_easy_setopt(curl, CURLOPT_URL, url); // Remplacez l'URL par l'URL de votre base de données
+        //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+        //"jdbc:mysql://localhost:3306/Meal?user=admin&password=password"
+        //https://pokebuildapi.fr/api/v1/pokemon
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "Erreur lors de la requête : %s\n", curl_easy_strerror(res));
+
+        gchar *response = g_strdup(curl_easy_escape(curl, url, 0));
+        gtk_label_set_text(GTK_LABEL(label), response);
+        g_free(response);
+
+        curl_easy_cleanup(curl);
+        
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     GtkWidget *button1, *button2;
     GtkWidget *vbox1;
     GtkWidget *box1;
     GtkWidget *entry1, *entry2, *entry3;
-
+    GtkWidget *label;
+    
+    //Allocation dynamique de la mémoire pour la variable globale
+    chaine = (char*)malloc(100*sizeof(char));
+    
     gtk_init(&argc, &argv);
 
     window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -170,9 +234,20 @@ int main(int argc, char *argv[]) {
     gtk_container_add(GTK_CONTAINER(window1), vbox1);
    
     
-    
+   
+	
 	gtk_widget_show_all(window1);
 	gtk_main();
+	
+	label = gtk_label_new("Hello, world!");
+	requete(label, global_result,chaine);
+	
+	/*int r= 0;
+	while(r < 2){
+		tab[r] = get_string();
+		g_print("Le nom de parametre chaine (main) est : %s\n", tab[r]);
+		r++;
+	}*/
 	
     return 0;
 }
